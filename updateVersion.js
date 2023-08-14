c2446a9022a91295c7b559cf24bd9a1ae7f2f4de
nour-karoui <nkaroui1998@gmail.com>
2023-08-14 17:28:36 +0100

    fix versioning issue

--- updateVersion.js
+++ updateVersion.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 
 function updateFileVersion () {
         const tokenList = JSON.parse(fs.readFileSync("./public/bioTokenList.json").toString())
+        const latestVersionTokenList = JSON.parse(fs.readFileSync("https://black-sky-1486.on.fleek.co/bioTokenList.json").toString())
         const updatedersion = {
         ...tokenList,
         version: {
             ...tokenList.version,
-            major: tokenList.version.major + 1
+            major: latestVersionTokenList.version.major + 1
         }
     }
     fs.writeFileSync("./public/bioTokenList.json", JSON.stringify(updatedersion))
