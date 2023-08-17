a30950d593099c18d82cdd0d582677e23bb18a70
Schmackofant <schmackofant@protonmail.com>
2023-08-17 17:57:00 +0200

    add bidding tokens

--- updateVersion.js
+++ updateVersion.js
@@ -1,6 +1,6 @@
 const {promises: fs} = require('node:fs');
 
-async function updateFileVersion () {
+async function updateBioTokenFileVersion () {
     const fileContent = await fs.readFile("./public/bioTokenList.json", "utf-8")
     const tokenList = JSON.parse(fileContent)
     const latestVersionTokenList = await (await fetch("https://bioxyz.on.fleek.co/bioTokenList.json")).json()
@@ -16,4 +16,21 @@ async function updateFileVersion () {
     await fs.writeFile("./public/bioTokenList.json", JSON.stringify(updatedVersion, null, 2))
 }
 
-updateFileVersion()
+async function updateBiddingTokenFileVersion () {
+    const fileContent = await fs.readFile("./public/biddingTokenList.json", "utf-8")
+    const tokenList = JSON.parse(fileContent)
+    const latestVersionTokenList = await (await fetch("https://bioxyz.on.fleek.co/biddingTokenList.json")).json()
+
+    const updatedVersion = {
+        ...tokenList,
+        version: {
+            ...tokenList.version,
+            major: latestVersionTokenList.version.major + 1
+        }
+    }
+    
+    await fs.writeFile("./public/biddingTokenList.json", JSON.stringify(updatedVersion, null, 2))
+}
+
+updateBioTokenFileVersion()
+updateBiddingTokenFileVersion()
